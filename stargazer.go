302ccfc7fe2b022a08bf6d35401a40de3b5fa4d7
Hoanh An <hoanhan@bennington.edu>
2019-12-20 21:38:45 -0500

    update api

--- stargazer.go
+++ stargazer.go
@@ -1,4 +1,4 @@
-package main
+package stargazer
 
 import (
 	"fmt"
@@ -15,7 +15,21 @@ type Repo struct {
 	Watch int    `json:"subscribers_count"`
 }
 
-func GetRepo(name string) *Repo {
+func Get(repos []string) {
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+
+	fmt.Fprintf(w, "%v\t%v\t%v\t%v\n", "name", "stars", "forks", "watch")
+
+	for _, repo := range repos {
+		r := getRepo(repo)
+		fmt.Fprintf(w, "%v\t%v\t%v\t%v\n", r.Name, r.Stars, r.Forks, r.Watch)
+	}
+
+	w.Flush()
+}
+
+func getRepo(name string) *Repo {
 	r := new(Repo)
 
 	err := request.GetJSON(fmt.Sprintf("https://api.github.com/repos/%s", name), r)
@@ -24,30 +38,4 @@ func GetRepo(name string) *Repo {
 	}
 
 	return r
-}
-
-func main() {
-	w := new(tabwriter.Writer)
-	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
-
-	repos := []string{
-		"hoanhan101/ultimate-go",
-		"hoanhan101/algo",
-		"hoanhan101/request",
-		"hoanhan101/dotfiles",
-		"hoanhan101/lang",
-		"vapor-ware/kubetest",
-		"vapor-ware/synse-server",
-		"Bennington-Distributed-Systems-2017/DarkDarkGo",
-		"bennington-hardware-hacking-2019/pos_system",
-	}
-
-	fmt.Fprintf(w, "%v\t%v\t%v\t%v\n", "name", "stars", "forks", "watch")
-
-	for _, repo := range repos {
-		r := GetRepo(repo)
-		fmt.Fprintf(w, "%v\t%v\t%v\t%v\n", r.Name, r.Stars, r.Forks, r.Watch)
-	}
-
-	w.Flush()
 }
