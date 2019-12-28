21e5c894c7cea3bda1a8737ada23e24de4e350ec
Hoanh An <hoanhan@bennington.edu>
2019-12-28 04:14:18 -0500

    count open issues

--- stargazer.go
+++ stargazer.go
@@ -9,21 +9,22 @@ import (
 )
 
 type Repo struct {
-	Name  string `json:"full_name"`
-	Stars int    `json:"stargazers_count"`
-	Forks int    `json:"forks"`
-	Watch int    `json:"subscribers_count"`
+	Name   string `json:"full_name"`
+	Stars  int    `json:"stargazers_count"`
+	Forks  int    `json:"forks"`
+	Watch  int    `json:"subscribers_count"`
+	Issues int    `json:"open_issues_count"`
 }
 
 func Get(repos []string) {
 	w := new(tabwriter.Writer)
 	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
 
-	fmt.Fprintf(w, "%v\t%v\t%v\t%v\n", "name", "stars", "forks", "watch")
+	fmt.Fprintf(w, "%v\t%v\t%v\t%v\t%v\n", "name", "stars", "forks", "watch", "issues")
 
 	for _, repo := range repos {
 		r := getRepo(repo)
-		fmt.Fprintf(w, "%v\t%v\t%v\t%v\n", r.Name, r.Stars, r.Forks, r.Watch)
+		fmt.Fprintf(w, "%v\t%v\t%v\t%v\t%v\n", r.Name, r.Stars, r.Forks, r.Watch, r.Issues)
 	}
 
 	w.Flush()
